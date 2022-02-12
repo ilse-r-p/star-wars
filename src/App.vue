8ae933069fdc428eb8eb5cd4f3a3e5a243da3fab
Ilse Rodríguez <ilse.rdzp@gmail.com>
2022-02-12 10:40:23 +0100

    WIP

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,26 @@
       type="search"
       placeholder="Am I your father?"
       class="border rounded w-1/4 focus:outline-none focus:ring focus:ring-yellow-300 px-2 py-1"
-      v-model="filter"
     />
   </div>
 
   <table class="m-auto">
     <thead>
       <tr class="bg-blue-100">
-        <th class="p-6">Name</th>
-        <th class="p-6">Height</th>
-        <th class="p-6">Mass</th>
-        <th class="p-6">Created</th>
-        <th class="p-6">Edited</th>
-        <th class="p-6">Planet name</th>
+        <th class="px-6 py-2">Name</th>
+        <th class="px-6 py-2">Height</th>
+        <th class="px-6 py-2">Mass</th>
+        <th class="px-6 py-2">Created</th>
+        <th class="px-6 py-2">Edited</th>
+        <th class="px-6 py-2">Planet name</th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="character in characters" v-bind:key="character">
+      <tr
+        v-for="character in characters"
+        v-bind:key="character"
+        :name="character.name"
+      >
         <td>{{ character.name }}</td>
         <td>{{ character.height }}</td>
         <td>{{ character.mass }}</td>
@@ -34,20 +37,32 @@
 
 <script lang="ts">
 import "./index.css";
+import axios from "axios";
 
 export default {
   data() {
     return {
+      // characters: {
+      //   name: { String },
+      //   height: { Number },
+      //   mass: { String },
+      //   created: { String },
+      //   edited: { String },
+      //   homeworld: { String },
+      // },
       characters: [],
-      created() {
-        fetch("https://swapi.dev/api/people")
-          .then((res) => res.json())
-          .then((res) => {
-            this.characters = res;
-            console.log(this.characters);
-          });
-      },
     };
+  },
+  created() {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((response) => {
+        console.log(response.data);
+        this.characters = response.data.push;
+      })
+      .catch((error) => {
+        console.log("error" + error.response);
+      });
   },
 };
 </script>
