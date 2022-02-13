3a5e6b9740bc19d56b35a131d3f2036bd2a2fa05
Ilse Rodríguez <ilse.rdzp@gmail.com>
2022-02-13 12:48:16 +0100

    Responsive

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -7,67 +7,122 @@
       class="border rounded w-3/4 md:w-1/4 focus:outline-none focus:ring focus:ring-yellow-400 px-2 py-1"
     />
   </div>
-  <table class="m-auto border border-gray-900">
+  <table class="mx-auto mx-2 xl:w-2/3 lg:border lg:border-gray-900 mb-16">
     <thead>
       <tr class="bg-gray-800 text-white text-left">
-        <th
-          class="px-6 py-2 cursor-pointer"
-          @click="sortBy('name')"
-          scope="col"
-        >
-          Name
-        </th>
-        <th
-          class="px-6 py-2 cursor-pointer"
-          @click="sortBy('height')"
-          scope="col"
-        >
+        <th class="px-6 py-2 cursor-pointer" @click="sortBy('name')">Name</th>
+        <th class="px-6 py-2 cursor-pointer" @click="sortBy('height')">
           Height
         </th>
-        <th
-          class="px-6 py-2 cursor-pointer"
-          @click="sortBy('mass')"
-          scope="col"
-        >
-          Mass
-        </th>
-        <th
-          class="px-6 py-2 cursor-pointer"
-          @click="sortBy('created')"
-          scope="col"
-        >
+        <th class="px-6 py-2 cursor-pointer" @click="sortBy('mass')">Mass</th>
+        <th class="px-6 py-2 cursor-pointer" @click="sortBy('created')">
           Created
         </th>
-        <th
-          class="px-6 py-2 cursor-pointer"
-          @click="sortBy('edited')"
-          scope="col"
-        >
+        <th class="px-6 py-2 cursor-pointer" @click="sortBy('edited')">
           Edited
         </th>
-        <th
-          class="px-6 py-2 cursor-pointer"
-          @click="sortBy('homeworld')"
-          scope="col"
-        >
+        <th class="px-6 py-2 cursor-pointer" @click="sortBy('homeworld')">
           Planet Name
         </th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(character, index) in sortedCharacters" v-bind:key="character.name">
-        <td v-html="highlightMatches(character.name)" class="px-6 py-2"></td>
-        <td class="px-6 py-2">{{ character.height }} cms</td>
-        <td class="px-6 py-2">{{ character.mass }}</td>
-        <td class="px-6 py-2">{{ dateTime(character.created) }}</td>
-        <td class="px-6 py-2">{{ dateTime(character.edited) }}</td>
-        <td class="px-6 py-2">{{ characterPlanet[index].name }}</td>
+      <tr
+        v-for="(character, index) in sortedCharacters"
+        v-bind:key="character.name"
+        class="border border-gray-800 lg:border-none"
+      >
+        <td v-html="highlightMatches(character.name)" class="px-6 lg:px-6 py-2 text-right lg:text-left"></td>
+        <td class="px-6 lg:px-6 py-2 text-right lg:text-left">{{ character.height }} cms</td>
+        <td class="px-6 lg:px-6 py-2 text-right lg:text-left">{{ character.mass }}</td>
+        <td class="px-6 lg:px-6 py-2 text-right lg:text-left">{{ dateTime(character.created) }}</td>
+        <td class="px-6 lg:px-6 py-2 text-right lg:text-left">{{ dateTime(character.edited) }}</td>
+        <td class="px-6 lg:px-6 py-2 text-right lg:text-left">{{ characterPlanet[index].name }}</td>
       </tr>
     </tbody>
   </table>
 </template>
+<style>
+/*
+	Max width before this PARTICULAR table gets nasty. This query will take effect for any screen smaller than 760px and also iPads specifically.
+	*/
+@media only screen and (max-width: 660px),
+  (min-device-width: 668px) and (max-device-width: 820px) {
+  /* Force table to not be like tables anymore */
+  table,
+  thead,
+  tbody,
+  th,
+  td,
+  tr {
+    display: block;
+  }
+
+  /* Hide table headers (but not display: none;, for accessibility) */
+  thead tr {
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+  }
+
+  tr {
+    margin: 0 0 1rem 0;
+  }
+
+  td {
+    border: none;
+    border-bottom: 1px solid #eee;
+    position: relative;
+    padding-left: 50%;
+  }
+
+  td:before {
+    width: 45%;
+    padding-right: 10px;
+    white-space: nowrap;
+  }
+
+  td:nth-of-type(1):before {
+    content: "Name";
+    position: absolute;
+    right: 50%;
+    text-align: left;
+  }
+  td:nth-of-type(2):before {
+    content: "Height";
+    position: absolute;
+    right: 50%;
+    text-align: left;
+  }
+  td:nth-of-type(3):before {
+    content: "Mass";
+    position: absolute;
+    right: 50%;
+    text-align: left;
+  }
+  td:nth-of-type(4):before {
+    content: "Created";
+    position: absolute;
+    right: 50%;
+    text-align: left;
+  }
+  td:nth-of-type(5):before {
+    content: "Edited";
+    position: absolute;
+    right: 50%;
+    text-align: left;
+  }
+  td:nth-of-type(6):before {
+    content: "Planet Name";
+    position: absolute;
+    right: 50%;
+    text-align: left;
+  }
+}
+</style>
 <script lang="ts">
 import moment from "moment";
+
 export default {
   data() {
     return {
--- src/views/AboutVue.vue
+++ src/views/AboutVue.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="about">
-    <div class="w-1/3 m-auto">
-      <h1 class="text-right text-xl">"Am I your father? The one, desiiiiireeeee..."</h1>
+    <div class="w-1/3 m-auto my-10">
+      <h1 class="text-right text-xl">
+        "Am I your father? The one, desiiiiireeeee..."
+      </h1>
+      <p class="text-right text-sm mt-4">- Ilse Rodríguez, 2022</p>
       <p class="text-right text-xs">
-        - Ilse Rodríguez, 2022 (singing this on a loop while completing the
-        challenge)
+        (singing this on a loop while completing the challenge)
       </p>
     </div>
   </div>
